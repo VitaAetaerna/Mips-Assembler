889d2297a83c15d945ea5e3e94ccaa9ed160cb6e
Leon² <75674735+VitaAetaerna@users.noreply.github.com>
2024-05-02 14:15:26 +0200

    Update Move.asm

--- Move.asm
+++ Move.asm
@@ -6,5 +6,5 @@ LUI $4, 10 # Savve 10 in Register 4
 SRL $4, $4, 16 # Shift value of Register 4 16 to right 
 # into Register 4
 
-MOVN $6, $5, $5 # Mov Reg 5 into Reg 6 if not Reg 5 == 0
-MOVN $5, $6, $4 # Mov Reg 4 into Reg 5 if not Reg 6 == 0
+MOVN $6, $5, $5 # Mov Reg 5 into Reg 6 if not Reg 5 != 0
+MOVN $5, $6, $4 # Mov Reg 4 into Reg 5 if Reg 6 != 0
