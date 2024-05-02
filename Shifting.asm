95750b539e688863dd98bfb78f80190e6c1c176d
Leon² <75674735+VitaAetaerna@users.noreply.github.com>
2024-05-02 13:56:08 +0200

    Create Shifting.asm

---
+++ Shifting.asm
@@ -0,0 +1,10 @@
+LUI $5, 15 # Savve 15 in Register 5
+SRL $5, $5, 16 # Shift value of Register 5 16 to right 
+# into Register 5
+
+
+SRL $5, $5, 2 # Shift value of Register 5 2 to right 
+# into Register 5
+
+SLL $5, $5, 1 # Shift value of Register 5 1 to left
+# into Register 5
