beff9fd37a24c0dbcc625af21b6b9a43294b0e7d
Leon² <75674735+VitaAetaerna@users.noreply.github.com>
2024-05-02 14:37:20 +0200

    Update Move.asm

--- Move.asm
+++ Move.asm
@@ -8,4 +8,7 @@ SRL $4, $4, 16 # Shift value of Register 4 16 to right
 # into Register 4
 
 MOVN $6, $5, $5 # Mov Reg 5 into Reg 6 if not Reg 5 != 0
-MOVN $5, $6, $4 # Mov Reg 4 into Reg 5 if Reg 6 != 0
+MOVN $5, $0, $6 # Mov Reg 4 into Reg 5 if Reg 6 != 0
+
+# movn rd rs rt
+# if rt ≠ 0 then rd ← rs
